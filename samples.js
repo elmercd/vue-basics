3371d0125fb7744dae65364f8df13856840ab101
Elmer Dadol <elmerecd@gmail.com>
2020-02-26 10:45:01 +0800

    Initial commit

---
+++ samples.js
@@ -0,0 +1,19 @@
+//   <input type="text" v-model="message">
+
+// Vue.component('randomizer', {
+//    template: `<div>
+//                <strong>{{ randomNumber }}</strong>
+//                <button @click="generateNumber">Click Me</button>
+//              </div>`,
+//    data: function() {
+//      return {
+//        randomNumber: 0
+//      }
+//    },
+//    methods: {
+//      // Return a random number between 1 and 100
+//      generateNumber: function() {
+//        this.randomNumber = Math.floor((Math.random() * 100) + 1);
+//      }
+//    }
+//  });
